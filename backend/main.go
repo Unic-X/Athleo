990e632bf7c0a1531e6b1f7c69d804b367f4d8bf
vanshjangir <vanshjangir0001@gmail.com>
2024-09-27 20:51:33 +0530

    added backend

---
+++ backend/main.go
@@ -0,0 +1,44 @@
+package main
+
+import(
+    "fmt"
+    "log"
+    "context"
+    "net/http"
+	"cloud.google.com/go/firestore"
+    "github.com/go-chi/chi/v5"
+    firebase "firebase.google.com/go"
+    "google.golang.org/api/option"
+)
+
+var fsClient *firestore.Client
+var ctx context.Context
+
+func setupFirestore(){
+    ctx = context.Background()
+    conf := &firebase.Config{ProjectID: "athleo-b82a3"}
+    sa := option.WithCredentialsFile("./athleo-pk.json")
+    app, err := firebase.NewApp(ctx, conf, sa)
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    client, err := app.Firestore(ctx)
+    if(err != nil){
+        log.Fatalln(err)
+    }
+
+    fsClient = client
+    fmt.Println("firestore setup successful")
+}
+
+
+func main(){
+    router := chi.NewRouter()
+    router.Get(
+        "/getroutes",
+        getRoutes,
+        )
+    setupFirestore()
+    http.ListenAndServe(":3000", router)
+}
