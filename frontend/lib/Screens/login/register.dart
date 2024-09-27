a3b2fe3f737a05b302b45eb0b420e7035ae26e0f
Unic-X <armansinghkshatri69@gmail.com>
2024-09-27 11:03:39 +0530

    Init with Login

---
+++ frontend/lib/Screens/login/register.dart
@@ -0,0 +1,32 @@
+import 'package:hack_space_temp/Screens/login/login.dart';
+import 'package:hack_space_temp/Screens/login/register_page.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  bool showLoginPage = true;
+
+  void togglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(
+        onTap: togglePages,
+      );
+    }
+    return Register_Page(
+      onTap: togglePages,
+    );
+  }
+}
---
+++ frontend/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+//import 'package:hack_space_temp/Screens/record.dart';
+//import 'package:hack_space_temp/Screens/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
+//import 'package:hack_space_temp/Screens/map.dart';
+import 'package:hack_space_temp/Screens/login/manage_user.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const Athleo());
+}
+
+class Athleo extends StatelessWidget {
+  const Athleo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const AuthPage(),
+      routes: {
+        //      '/map': (context) => const MapScreen(),
+        //      '/record': (context) => const RecordScreen(),
+      },
+      title: "Fence Mate",
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+    );
+  }
+}
